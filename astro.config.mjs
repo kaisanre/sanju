975b6805a06bacb44c0d59cea92b2da1b229388e
kaisanre <worldseekerace@gmail.com>
2025-03-07 22:14:51 +0530

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
-import cloudflare from "@astrojs/cloudflare";
+import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
@@ -16,6 +16,6 @@ export default defineConfig({
 		}),
 		tailwind(),
 	],
-	output: "static",
-	adapter: cloudflare(),
+	output: "server",
+	adapter: vercel(),
 });
